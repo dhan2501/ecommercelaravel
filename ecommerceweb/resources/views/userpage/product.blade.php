95b276bd2739a538f17d5b6c36bb29e289e790a7
dhan2501 <guptadhananjay10@gmail.com>
2023-12-24 15:32:52 +0530

    product add in slider

--- ecommerceweb/resources/views/userpage/product.blade.php
+++ ecommerceweb/resources/views/userpage/product.blade.php
@@ -1,9 +1,32 @@
-@extends("userpage/master")
-@section("content")
+@extends("userpage/master") @section("content")
 
-<div class="container">
-    <h1>Product List</h1>
+<div class="container custom-product">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            @foreach($products as $item)
+            <div class="carousel-item {{$item['id']==1?'active':''}}">
+                <img src="{{$item['gallery']}}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>{{$item['name']}}</h5>
+                    <p>{{$item['description']}}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+    </div>
+ 
 </div>
-
-
 @endsection
